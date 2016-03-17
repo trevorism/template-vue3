52b111fe301601596ffd19c296c83bbf6a73fd67
Trevor Brooks <trevorism@gmail.com>
2016-03-17 02:40:45 -0400

    Fix issue with updater

--- src/main/groovy/com/trevorism/gcloud/webapi/CrudController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/CrudController.groovy
@@ -1,16 +1,8 @@
 package com.trevorism.gcloud.webapi
 
-import com.google.appengine.api.datastore.DatastoreService
-import com.google.appengine.api.datastore.DatastoreServiceFactory
 import com.google.appengine.api.datastore.Entity
-import com.google.appengine.api.datastore.FetchOptions
-import com.google.appengine.api.datastore.Key
-import com.google.appengine.api.datastore.KeyFactory
-import com.google.appengine.api.datastore.PreparedQuery
-import com.google.appengine.api.datastore.Query
 import com.trevorism.gcloud.dao.CrudDatastoreDAO
 import com.trevorism.gcloud.dao.DatastoreDAO
-import org.json.JSONObject
 
 import javax.ws.rs.Consumes
 import javax.ws.rs.DELETE
@@ -48,7 +40,7 @@ class CrudController {
     @Path("{kind}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    Entity create(@PathParam("kind") String kind, def data){
+    Entity create(@PathParam("kind") String kind, Map<String, Object> data){
         DatastoreDAO dao = new CrudDatastoreDAO(kind)
         def entity = dao.create(data)
         return entity
@@ -59,7 +51,7 @@ class CrudController {
     @Path("{kind}/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    Entity update(@PathParam("kind") String kind, @PathParam("id") long id, JSONObject data){
+    Entity update(@PathParam("kind") String kind, @PathParam("id") long id, Map<String, Object> data){
         DatastoreDAO dao = new CrudDatastoreDAO(kind)
         def entity = dao.update(id, data)
         return entity
