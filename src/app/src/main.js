6daa32d417e798c5b6fe620b648726dd5103d01a
Trevor Brooks <trevorism@gmail.com>
2023-12-25 09:59:44 -0500

    template3 updates to add mixpanel, update dependencies, and fix a bug with the base in vite

--- src/app/src/main.js
+++ src/app/src/main.js
@@ -4,9 +4,18 @@ import App from './App.vue'
 import VueClickAway from 'vue3-click-away'
 import { createVuestic } from 'vuestic-ui'
 import config from '../vuestic.config.js'
+import VueMixpanel from 'vue-mixpanel'
 import './style.css'
 
 const app = createApp(App)
 app.use(VueClickAway)
 app.use(createVuestic({ config }))
+app.use(VueMixpanel, {
+  token: "3ad96fc246692627d5addd73aa6072ae",
+  config: {
+    track_pageview: true,
+    secure_cookie: true,
+    same_site: 'None'
+  },
+})
 app.mount('#app')
