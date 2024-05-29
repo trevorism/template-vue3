1848f588822433399ae4a47e2daedc93688b825f
Trevor Brooks <trevorism@gmail.com>
2024-05-28 21:36:44 -0400

    Update template with latest event pushing build

--- src/main/groovy/com/trevorism/Application.groovy
+++ src/main/groovy/com/trevorism/Application.groovy
@@ -5,15 +5,23 @@ import io.micronaut.runtime.Micronaut
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @OpenAPIDefinition(
         info = @Info(
-                contact = @Contact(url = "https://trevorism.com")
+                title = "App",
+                version = "0.0.1",
+                description = "API",
+                contact = @Contact(url = "https://trevorism.com", name = "Trevor Brooks", email = "tbrooks@trevorism.com")
         )
 )
 @CompileStatic
 class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application)
+
     static void main(String[] args) {
+        log.info("Started application")
         Micronaut.run(Application, args)
     }
 }
